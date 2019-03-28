c842ed064c847001ea2e890b216e50da4728c789
Jim Borden <jim.borden@couchbase.com>
2019-03-28 09:54:46 +0900

    macOS build script

---
+++ build_deps_macos.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+which git > /dev/null || (echo "git not found; aborting..."; exit 1)
+which cmake > /dev/null || (echo "cmake not found; aborting..."; exit 1)
+which brew > /dev/null || (echo "homebrew not found; aborting..."; exit 1)
+
+pushd `dirname $0`
+if [[ ! -d libfido2 ]]; then
+    git clone https://github.com/yubico/libfido2
+fi
+
+mkdir -p libfido2/build
+pushd libfido2/build
+
+if [[ ! -d libcbor ]]; then
+    git clone https://github.com/pjk/libcbor --branch v0.5.0
+fi
+
+mkdir -p libcbor/build
+pushd libcbor/build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j8
+echo "Need sudo rights to install libcbor..."
+sudo make install
+popd
+
+brew update
+brew upgrade openssl@1.1
+brew switch openssl@1.1 1.1.1b
+brew install mandoc
+
+export PKG_CONFIG_PATH=/usr/local/opt/openssl@1.1/lib/pkgconfig
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j8
+make man_symlink_html
+echo "Need sudo rights to install libfido2..."
+sudo make install
+
+popd
+popd
