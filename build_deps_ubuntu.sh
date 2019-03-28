b635b835772e5866547711f7c6326ed87503eeb1
Jim Borden <jim.borden@couchbase.com>
2019-03-28 10:18:55 +0900

    Ubuntu deps build script (#1)

---
+++ build_deps_ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+  
+which git > /dev/null || (echo "git not found; aborting..."; exit 1)
+which cmake > /dev/null || (echo "cmake not found; aborting..."; exit 1)
+
+pushd `dirname $0`
+if [[ ! -d libfido2 ]]; then
+    git clone https://github.com/yubico/libfido2
+fi
+
+mkdir -p libfido2/build
+pushd libfido2/build
+
+if [[ ! -d libcbor ]]; then
+    git clone https://github.com/pjk/libcbor --branch v0.5.0
+fi
+
+mkdir -p libcbor/build
+pushd libcbor/build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j8
+echo "Need sudo rights to install libcbor..."
+sudo make install
+popd
+
+echo "Need sudo rights to install apt-get packages..."
+sudo apt-get update
+sudo apt-get install -y pkg-config libssl-dev libudev-dev
+
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j8
+echo "Need sudo rights to install libfido2..."
+sudo make install
+
+popd
+popd
